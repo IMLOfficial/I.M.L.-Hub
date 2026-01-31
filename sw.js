777b23c8ec41a47a65888780008ff32543d2386a
IMLOfficial <marian.leauta@gmx.de>
2026-01-31 21:05:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const CACHE="iml-v3";
-const ASSETS=[
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./logo.png"
-];
+const CACHE="iml-v1";
+const ASSETS=["./","index.html","logo.png"];
 
 self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(k=>Promise.all(
-      k.filter(x=>x!==CACHE).map(x=>caches.delete(x))
-    ))
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      );
+    })
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch",e=>{
   e.respondWith(
-    fetch(e.request).catch(()=>caches.match(e.request))
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
