e6ecc523b2104ab02ea09d80abacff08d103ac58
IMLOfficial <marian.leauta@gmx.de>
2026-04-26 01:18:56 +0200

    Refresh cache after removing theme button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="iml-v11";
+const CACHE="iml-v12";
 const ASSETS=["./","./index.html","./manifest.json","./logo.svg","./video-inspired-bg.js"];
 
 self.addEventListener("install",event=>{
