f699702f01acdaedba3f0e9890bcce216deaaa7f
IMLOfficial <marian.leauta@gmx.de>
2026-04-26 01:59:38 +0200

    Bump cache for header refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="iml-v18";
+const CACHE="iml-v19";
 const ASSETS=["./","./index.html","./manifest.json","./logo.svg","./video-inspired-bg.js"];
 
 self.addEventListener("install",event=>{
