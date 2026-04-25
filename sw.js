04e00f0b0ab7170036f4a4e18cde5bb361ada4f7
IMLOfficial <marian.leauta@gmx.de>
2026-04-26 01:48:17 +0200

    Refresh cache for mobile video fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="iml-v15";
+const CACHE="iml-v16";
 const ASSETS=["./","./index.html","./manifest.json","./logo.svg","./video-inspired-bg.js"];
 
 self.addEventListener("install",event=>{
