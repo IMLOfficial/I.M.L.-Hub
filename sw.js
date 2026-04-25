84254eb9a60af28bafe00787878791b7c818ac90
IMLOfficial <marian.leauta@gmx.de>
2026-04-26 00:58:55 +0200

    Refresh cache for interactive hub features

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="iml-v9";
+const CACHE="iml-v10";
 const ASSETS=["./","./index.html","./manifest.json","./logo.svg","./video-inspired-bg.js"];
 
 self.addEventListener("install",event=>{
