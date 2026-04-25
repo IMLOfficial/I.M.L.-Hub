e0ceafdb7989c4e0f39db2d6dc4e6fe43e2ffc01
IMLOfficial <marian.leauta@gmx.de>
2026-04-26 01:54:56 +0200

    Bump site cache after mobile optimizations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="iml-v16";
+const CACHE="iml-v17";
 const ASSETS=["./","./index.html","./manifest.json","./logo.svg","./video-inspired-bg.js"];
 
 self.addEventListener("install",event=>{
